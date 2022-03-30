3143621367a10a4860429fecc5f69d862ce8ac44
Pablo <dalbem@gmail.com>
2022-03-30 14:04:27 -0300

    initial commit

---
+++ Heranca/src/Pessoa.java
@@ -0,0 +1,40 @@
+public abstract class Pessoa {
+    private String endereco;
+    private String telefone;
+    private String email;
+
+    //método abstrato. Classes filhas (subclasses) deverão implementá-lo
+    public abstract void mostrarDados();
+
+    public Pessoa(String endereco, String telefone, String email) {
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.email = email;
+    }
+    public Pessoa() {
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ Heranca/src/PessoaFisica.java
@@ -0,0 +1,42 @@
+public  class PessoaFisica extends Pessoa{
+    private String cpf;
+    private String nome;
+
+    //Implementação do método abstrato mostrarDados()
+    @Override
+    public void mostrarDados() {
+        System.out.println("\n CPF: "+ cpf
+                + "\n Nome: "+ nome
+                + "\n E-mail: "+ getEmail());
+    }
+
+
+    public PessoaFisica(String cpf, String nome, String endereco, String telefone, String email) {
+        super(endereco, telefone, email);
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public boolean validarCPF(){
+        //código para validar CPF
+        return false;
+    }
+
+
+}
